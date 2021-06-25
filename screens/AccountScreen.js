a50ebc5b430c4f06077c7f23bfd6847780d2e82d
AtharvaPanegai <panegai.atharv@gmail.com>
2021-06-25 16:12:14 +0530

    Sixth Commit

--- screens/AccountScreen.js
+++ screens/AccountScreen.js
@@ -1,16 +1,99 @@
-import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
-import PodcastCircleComponent from '../components/PodcastCircleComponent'
+/** @format */
 
+import React from "react";
+import { StyleSheet, Text, View,Image } from "react-native";
+import PodcastCircleComponent from "../Components/PodcastCircleComponent";
+import { WindowHeight, WindowWidth } from "../Dimensions";
 
-const AccountScreen = () => {
-    return (
-        <View>
-           <PodcastCircleComponent image = "https://randomuser.me/api/portraits/men/86.jpg" />
+const AccountScreen = ({ posts, followers, following, username,postimage }) => {
+  return (
+    <View style={styles.container}>
+      <View>
+        <PodcastCircleComponent image='https://randomuser.me/api/portraits/men/86.jpg' />
+        <Text style={styles.userName}> Username</Text>
+      </View>
+      <View style={styles.followersContainer}>
+        <View style={styles.singleContainer}>
+          <Text style={styles.accountParamHeading}>Posts</Text>
+          <Text style={styles.accountParamNumber}> 100 </Text>
         </View>
-    )
-}
+        <View style={styles.singleContainer}>
+          <Text style={styles.accountParamHeading}>Following</Text>
+          <Text style={styles.accountParamNumber}> 200 </Text>
+        </View>
+        <View style={styles.singleContainer}>
+          <Text style={styles.accountParamHeading}>Followers</Text>
+          <Text style={styles.accountParamNumber}> 300 </Text>
+        </View>
+        <View style={styles.singleContainer}></View>
+      </View>
+      <View style = {styles.postContainer}>
+        <View style = {styles.posts}>
+            <Image style = {styles.postImage} source = {{uri : postimage || "https://image.flaticon.com/icons/png/512/985/985955.png" }} />
+            <Image style = {styles.postImage} source = {{uri : postimage || "https://image.flaticon.com/icons/png/512/985/985955.png" }} />
+            <Image style = {styles.postImage} source = {{uri : postimage || "https://image.flaticon.com/icons/png/512/985/985955.png" }} />
+        </View>
+        <View style = {styles.posts}>
+            <Image style = {styles.postImage} source = {{uri : postimage || "https://image.flaticon.com/icons/png/512/985/985955.png" }} />
+            <Image style = {styles.postImage} source = {{uri : postimage || "https://image.flaticon.com/icons/png/512/985/985955.png" }} />
+            <Image style = {styles.postImage} source = {{uri : postimage || "https://image.flaticon.com/icons/png/512/985/985955.png" }} />
+        </View>
+        <View style = {styles.posts}>
+            <Image style = {styles.postImage} source = {{uri : postimage || "https://image.flaticon.com/icons/png/512/985/985955.png" }} />
+            <Image style = {styles.postImage} source = {{uri : postimage || "https://image.flaticon.com/icons/png/512/985/985955.png" }} />
+            <Image style = {styles.postImage} source = {{uri : postimage || "https://image.flaticon.com/icons/png/512/985/985955.png" }} />
+        </View>
+      </View>
+    </View>
+  );
+};
 
-export default AccountScreen
+export default AccountScreen;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: 75,
+    backgroundColor: "black",
+  },
+  followersContainer: {
+    flexDirection: "row",
+  },
+  singleContainer: {},
+  accountParamHeading: {
+    fontSize: 16,
+    fontWeight: "bold",
+    marginHorizontal: WindowWidth * 0.09,
+    color: "white",
+  },
+  accountParamNumber: {
+    fontSize: 16,
+    fontWeight: "300",
+    marginHorizontal: WindowWidth * 0.1,
+    color: "white",
+  },
+  userName: {
+    color: "white",
+    justifyContent: "center",
+    alignItems: "center",
+    marginLeft: WindowWidth * 0.399,
+    paddingBottom: 50,
+  },
+  postContainer : {
+    marginTop : WindowHeight*0.03,
+
+  },
+  posts : {
+      flexDirection : "row",
+
+  },
+  postImage : {
+      width : WindowWidth*0.3,
+      height : WindowWidth*0.3,
+      marginHorizontal : WindowWidth*0.01,
+      marginTop : WindowHeight*0.01,
+      borderWidth : 0.1,
+      borderColor : "white",
+      
+  }
+});
--- screens/NotificationScreen.js
+++ screens/NotificationScreen.js
@@ -1,14 +1,72 @@
-import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+/** @format */
+
+import React from "react";
+import { StyleSheet, Text, View, Image } from "react-native";
 
 const NotificationScreen = () => {
-    return (
-        <View>
-            <Text>Hi I Am Notification Screen!!</Text>
-        </View>
-    )
-}
+  return (
+    <View style={styles.main}>
+      <View style={styles.container}>
+        <Image
+          style={styles.image}
+          source={{ uri: "https://randomuser.me/api/portraits/men/86.jpg" }}
+        />
+        <Text style={styles.text}>Harrypotter just followed you</Text>
+      </View>
 
-export default NotificationScreen
+      <View style={styles.container}>
+        <Image
+          style={styles.image}
+          source={{ uri: "https://randomuser.me/api/portraits/men/86.jpg" }}
+        />
+        <Text style={styles.text}>
+          Harrypotter just followed you
+        </Text>
+      </View>
+      <View style={styles.container}>
+        <Image
+          style={styles.image}
+          source={{ uri: "https://randomuser.me/api/portraits/men/86.jpg" }}
+        />
+        <Text style={styles.text}>
+            Harrypotter just followed you
+        </Text>
+      </View>
+      <View style={styles.container}>
+      <Image
+          style={styles.image}
+          source={{ uri: "https://randomuser.me/api/portraits/men/86.jpg" }}
+        />
+        <Text style={styles.text}>
+            Harrypotter just followed you
+        </Text>
+      </View>
+    </View>
+  );
+};
 
-const styles = StyleSheet.create({})
+export default NotificationScreen;
+
+const styles = StyleSheet.create({
+  container: {
+
+    flexDirection: "row",
+    paddingVertical: 15,
+    alignItems: "center",
+    paddingHorizontal: 20,
+    borderTopColor: "gray",
+
+  },
+  main: {
+    paddingTop: 50,
+    backgroundColor : "black",
+    flex : 1,
+  },
+  image:{
+     height: 70, width: 70, borderRadius: 50 
+  },
+  text:{
+      paddingHorizontal:20,
+      color:"white"
+  }
+});
